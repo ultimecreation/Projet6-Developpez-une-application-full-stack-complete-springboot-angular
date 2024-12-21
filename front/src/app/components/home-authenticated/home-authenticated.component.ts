1650331cf7d479b072938d51e2c53ad7d8f11320
ultimecreation <naserbechata@gmail.com>
2024-12-21 15:58:41 +0100

    added feed list

--- front/src/app/components/home-authenticated/home-authenticated.component.ts
+++ front/src/app/components/home-authenticated/home-authenticated.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FeedListComponent } from "../feed-list/feed-list.component";
 
 @Component({
-  selector: 'app-home-authenticated',
-  imports: [],
-  templateUrl: './home-authenticated.component.html',
-  styleUrl: './home-authenticated.component.scss'
+    selector: 'app-home-authenticated',
+    imports: [FeedListComponent],
+    templateUrl: './home-authenticated.component.html',
+    styleUrl: './home-authenticated.component.scss'
 })
 export class HomeAuthenticatedComponent {
 
