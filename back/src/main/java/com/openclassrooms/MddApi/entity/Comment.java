f0777ace7c2d47ddb31a4cf86a1774f3ef73482c
ultimecreation <naserbechata@gmail.com>
2024-12-21 21:08:56 +0100

    created entities

---
+++ back/src/main/java/com/openclassrooms/MddApi/entity/Comment.java
@@ -0,0 +1,39 @@
+package com.openclassrooms.MddApi.entity;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @JsonFormat(pattern = "yyyy-dd-MM")
+    private LocalDateTime created_at;
+}
---
+++ back/src/main/java/com/openclassrooms/MddApi/entity/Post.java
@@ -0,0 +1,42 @@
+package com.openclassrooms.MddApi.entity;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "posts")
+@Data
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @JsonFormat(pattern = "yyyy-dd-MM")
+    private LocalDateTime created_at;
+}
