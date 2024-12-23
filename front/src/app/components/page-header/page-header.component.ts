c1b13ce8dba40ec8b32e620bf55581a3fb7447be
ultimecreation <naserbechata@gmail.com>
2024-12-23 18:48:10 +0100

    updated page header

--- front/src/app/components/page-header/page-header.component.ts
+++ front/src/app/components/page-header/page-header.component.ts
@@ -1,18 +1,16 @@
 import { Location } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
-import { Router } from '@angular/router';
-import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
     selector: 'app-page-header',
-    imports: [NavbarComponent],
+    imports: [],
     templateUrl: './page-header.component.html',
     styleUrl: './page-header.component.scss'
 })
 export class PageHeaderComponent {
 
     title = input<string>('')
-    invisible = input<boolean>(false)
+    goBackBtnInvisible = input<boolean>(false)
 
     private location = inject(Location)
 
