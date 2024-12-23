b1a0cfe5715c570b467711e6ead2d105baedf4e3
ultimecreation <naserbechata@gmail.com>
2024-12-23 22:03:32 +0100

    Update app.routes.ts

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -7,12 +7,14 @@ import { ArticlesComponent } from './pages/articles/articles.component';
 import { ThemesComponent } from './pages/themes/themes.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guards/auth.guard';
+import { AddArticleComponent } from './pages/articles/add-article/add-article.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'articles', component: ArticlesComponent, canActivate: [authGuard] },
+    { path: 'articles/add', component: AddArticleComponent, canActivate: [authGuard] },
     { path: 'themes', component: ThemesComponent, canActivate: [authGuard] },
     { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
 ];
