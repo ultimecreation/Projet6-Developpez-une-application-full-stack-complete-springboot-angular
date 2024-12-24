57780bf0591ee785ab5f4a40a416e2c205a6cc3f
ultimecreation <naserbechata@gmail.com>
2024-12-24 13:13:30 +0100

    Update UserController.java

--- back/src/main/java/com/openclassrooms/MddApi/controller/UserController.java
+++ back/src/main/java/com/openclassrooms/MddApi/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,9 +33,9 @@ import com.openclassrooms.MddApi.services.UserService;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/user")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
@@ -130,7 +129,7 @@ public class UserController {
      * @param authentication
      * @return ResponseEntity<ApiResponse>
      */
-    @PostMapping("/topics/add/{id}")
+    @GetMapping("/topics/add/{id}")
     public ResponseEntity<ApiResponse> addUserTopic(@PathVariable int id, Authentication authentication) {
 
         int userId = ((User) authentication.getPrincipal()).getId();
